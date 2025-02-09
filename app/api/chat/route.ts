0579542b2993fe60d8a50d83ca6437e937fe1c92
Aman Gupta <00amangpt00@gmail.com>
2025-02-09 19:46:35 +0530

    Removed unwanted code

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,46 +10,9 @@ export async function POST(req: Request) {
     temperature: 0.7,
     maxTokens: 500,
     system: `You are a super assistant that acts as a orchestrator for different agents. For any blockchain related queries, you should invoke the askCDP tool. When you are asked to swap tokens, you invoke the swapTokens tool.`,
-    tools: tools, 
+    tools: tools,
     toolChoice: "auto",
     maxSteps: 5,
   });
   return result.toDataStreamResponse();
 }
-
-// import { ChatOpenAI } from "@langchain/openai";
-// import { LangChainAdapter } from "ai";
-// import { tool } from "@langchain/core/tools";
-// import { z } from "zod";
-// export const maxDuration = 60;
-
-// export async function POST(req: Request) {
-//   const { prompt } = await req.json();
-
-//   const getStockPrice = tool(
-//     async ({ symbol }) => {
-//       console.log("Stock price tool called");
-//       return { symbol, price: 100 };
-//     },
-//     {
-//       name: "getStockPrice",
-//       description: "Get the price of a stock",
-//       schema: z.object({
-//         symbol: z.string().describe("The stock symbol to get the price for"),
-//       }),
-//     }
-//   );
-
-//   const tools = [getStockPrice];
-
-//   const model = new ChatOpenAI({
-//     modelName: "llama70b",
-//     configuration: {
-//       baseURL: "https://llama70b.gaia.domains/v1",
-//     },
-//   }).bindTools(tools);
-
-//   const stream = await model.stream(prompt);
-
-//   return LangChainAdapter.toDataStreamResponse(stream);
-// }
